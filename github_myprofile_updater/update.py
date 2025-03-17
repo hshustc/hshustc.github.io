7cd16174955ae1f10d3348672763e1b219db58bf
hshustc <hshustc@gmail.com>
2025-03-17 16:16:46 +0800

    Fix Updater Bugs

--- github_myprofile_updater/update.py
+++ github_myprofile_updater/update.py
@@ -1,9 +1,9 @@
 if __name__ == '__main__':
     _header = '## Hi there 👋'
     base_dir = '../_pages/includes/'
-    _intro = open(f'{base_dir}/1-intro.md').read().strip()
-    with open('README.md', 'w') as f:
+    _intro = open(f'{base_dir}/1-intro.md', encoding='utf8').read().strip()
+    with open('README.md', 'w', encoding='utf8') as f:
         f.write(_header)
         f.write('\n\n')
         f.write(_intro)
-        f.write('\n\n##')
+        f.write('\n\n')
