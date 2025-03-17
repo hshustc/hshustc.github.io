857c85840f9aac971a0e147dc60531df63bcb0e9
hshustc <hshustc@gmail.com>
2025-03-17 19:23:11 +0800

    Remove Update Flow

--- _pages/includes/homepage.md
+++
@@ -1,3 +0,0 @@
-# Homepages
-- Personal Pages: https://hshustc.github.io (updated recently)
-- Google Scholar: https://scholar.google.com/citations?user=6gnHaLcAAAAJ&hl
--- github_myprofile_updater/update.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    _header = '## Hi there'
-    base_dir = '../_pages/includes/'
-    _homepage = open(f'{base_dir}/homepage.md').read().strip()
-    with open('README.md', 'w') as f:
-        f.write(_header)
-        f.write('\n\n')
-        f.write(_homepage)
